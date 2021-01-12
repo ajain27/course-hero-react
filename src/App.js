3fe5c690982cb4f291c7a5f2559c955781b0515f
Ankit Jain <ankit.jain67@t-mobile.com>
2021-01-12 14:13:36 -0800

    Adding the functionality for enter press and displaying the data

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import CourseLookUp from './components/CourseLookUp'
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header"></header> */}
       <CourseLookUp></CourseLookUp>
     </div>
   );
